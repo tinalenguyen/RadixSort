ba487917a6fe026bc8d7ef73e58e016b78a95cbd
tinalenguyen <>
2021-01-12 13:16:01 -0500

    fixed nth

--- Radix.java
+++ Radix.java
@@ -1,19 +1,23 @@
 public class Radix{
 
+
     public static int nth(int n, int col){
 //get nth digit of an int, where 0 is the ones column, 1 is the tens
-      return (n % (Math.pow(10,col) ));
+      int temp =  (int) (n / Math.pow(10,col) );
+      int ans = (int) ( temp % 10);
+    //  int ans = (int) Math.round( (n / (Math.pow(10,col) ) ) % (Math.pow(10,col) ) );
+      return ans;
 
     }
 
     public static int length(int n){
 //return the number of digits in n
-
+      return (int)(Math.log(n));
     }
 
-    public static void merge(MyLinkedList original, MyLinkedList][]buckets){
+    public static void merge(MyLinkedList original, MyLinkedList[] buckets){
 //merge all the linked lists in the bucket array into your original linked list
-
+      String filler = "";
     }
 
 
