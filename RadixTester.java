0eac13df2391e7a3985bb9fa28c3c22ea601be5f
tinalenguyen <>
2021-01-12 19:30:06 -0500

    tested length as well

--- RadixTester.java
+++ RadixTester.java
@@ -6,9 +6,13 @@ public class RadixTester{
     int z = 783466;
 
     Radix test = new Radix();
-    System.out.println(test.nth(x, 4));
-    System.out.println(test.nth(y, 0));
+    System.out.println(test.nth(x, 3));
+    System.out.println(test.nth(y, 2));
+    System.out.println(test.nth(z, 5));
 
+    System.out.println(test.length(x));
+    System.out.println(test.length(y));
+    System.out.println(test.length(z));
   }
 
 
