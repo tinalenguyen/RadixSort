f6099dd9aa5479103b83a385971bab17c6d1f606
tinalenguyen <>
2021-01-13 00:04:11 -0500

    got radixsimplesort working

--- Radix.java
+++ Radix.java
@@ -25,5 +25,50 @@ public class Radix{
       }
     }
 
+    public static void radixSortSimple(SortableLinkedList data){
+//sorts non negative integer values / assume there are no negative values
+      int longestLength = 0;
+
+      for (int i = 0 ; i < data.size() ; i++){
+        if (length(data.get(i)) > longestLength ) longestLength = length(data.get(i));
+      }
+
+      for (int i = 0 ; i < longestLength ; i++){
+        SortableLinkedList bucket0 = new SortableLinkedList();
+        SortableLinkedList bucket1 = new SortableLinkedList();
+        SortableLinkedList bucket2 = new SortableLinkedList();
+        SortableLinkedList bucket3 = new SortableLinkedList();
+        SortableLinkedList bucket4 = new SortableLinkedList();
+        SortableLinkedList bucket5 = new SortableLinkedList();
+        SortableLinkedList bucket6 = new SortableLinkedList();
+        SortableLinkedList bucket7 = new SortableLinkedList();
+        SortableLinkedList bucket8 = new SortableLinkedList();
+        SortableLinkedList bucket9 = new SortableLinkedList();
+
+        for (int j = 0 ; j < data.size() ; j++){
+          if (nth(data.get(j),i) == 0) bucket0.add(data.get(j));
+          else if (nth(data.get(j),i) == 1) bucket1.add(data.get(j));
+          else if (nth(data.get(j),i) == 2) bucket2.add(data.get(j));
+          else if (nth(data.get(j),i) == 3) bucket3.add(data.get(j));
+          else if (nth(data.get(j),i) == 4) bucket4.add(data.get(j));
+          else if (nth(data.get(j),i) == 5) bucket5.add(data.get(j));
+          else if (nth(data.get(j),i) == 6) bucket6.add(data.get(j));
+          else if (nth(data.get(j),i) == 7) bucket7.add(data.get(j));
+          else if (nth(data.get(j),i) == 8) bucket8.add(data.get(j));
+          else bucket9.add(data.get(j));
+
+        }
+        SortableLinkedList[] buckets = {bucket1,bucket2,bucket3,bucket4,bucket5,bucket6,bucket7,bucket8,bucket9};
+        Radix.merge(bucket0, buckets);
+        //clear data
+        int h = data.size()-1;
+        while (h >=0){
+          data.remove(h);
+          h--;
+        }
+        //combine for sorting again
+        data.extend(bucket0);
+    }
+  }
 
 }
